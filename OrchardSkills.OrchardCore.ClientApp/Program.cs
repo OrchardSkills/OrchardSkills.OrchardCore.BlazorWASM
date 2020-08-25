b803273362ccd8a0247865f7d221b627adf15879
sales <sales@angularadvisors.com>
2020-08-24 22:42:25 -0600

    Working with click counter

---
+++ OrchardSkills.OrchardCore.ClientApp/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OrchardSkills.OrchardCore.ClientApp.Pages;
+
+namespace OrchardSkills.OrchardCore.ClientApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<Counter>("my-counter");
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
